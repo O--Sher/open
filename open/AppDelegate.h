88e617fd811cc655b7b61f92c17322625ed1243d
O_Sher <Skoolblako@gmail.com>
2014-11-10 17:36:07 +0200

    Initial Commit

---
+++ open/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  open
+//
+//  Created by O_Sher on 10.11.14.
+//  Copyright (c) 2014 OSher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ open/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  open
+//
+//  Created by O_Sher on 10.11.14.
+//  Copyright (c) 2014 OSher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
